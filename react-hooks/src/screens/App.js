58ef1a48224a41b75b4fbf232a91e953fb9be4af
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2025-07-11 09:47:20 -0600

    Estructura

---
+++ react-hooks/src/screens/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Index</h1>
+      <a href="useCallback">Route</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-hooks/src/screens/useMemo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function useMemo() {
+  return (
+    <div>useMemo</div>
+  )
+}
